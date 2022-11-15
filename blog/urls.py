9489c9a98a892c82f9d1aaf2137607117b7ad048
erastusnzula <nzulaerastus@gmail.com>
2022-11-15 17:39:24 +0300

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from blog.views.index_page import IndexPage, PostDetails
 from blog.views.category import PostsByCategory
+from blog.views.downloads import AllDownloads
 
 app_name = 'blog'
 urlpatterns = [
     path('', IndexPage.as_view(), name='index-page'),
     path('category/<category>/', PostsByCategory.as_view(), name='category'),
     path('post/<int:id>/', PostDetails.as_view(), name='post-details'),
+    path('apps/download/', AllDownloads.as_view(), name='downloads'),
 ]
--- blog/views/downloads.py
+++ blog/views/downloads.py
@@ -1,5 +1,14 @@
 from blog.models.downloads import Download
+from django.views import View
+from django.shortcuts import render
 
+class AllDownloads(View):
+    def get(self, *args, **kwargs):
+        all_downloads = Download.objects.all()
+        context = {
+            'all_downloads': all_downloads
+        }
+        return render(self.request, 'all_downloads.html', context)
 
 def all_downloads(request):
     downloads = Download.objects.all()
